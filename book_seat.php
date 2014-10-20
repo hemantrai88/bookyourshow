850c4019138cf4ea2e22c5bbbf463dc78f4f07d1
hemantrai88 <hemantrai1988@gmail.com>
2014-10-20 22:37:50 +0530

    Work from office

--- book_seat.php
+++ book_seat.php
@@ -9,19 +9,36 @@ if (mysqli_connect_errno()) {
 	$show_id=$_POST['show_id'];
 	$seat_id=$_POST['seat_id'];
 	$timing=$_POST['timing'];
+	$email=$_POST['email'];
+
+		$res = mysqli_query($con,"SELECT * FROM bookings WHERE show_id=".$show_id." AND timing_id=".$timing." AND seat_id=".$seat_id);
 	
-	if(mysqli_query($con, "INSERT INTO bookings (`show_id`, `timing_id`, `seat_id`) VALUES ($show_id, $timing, $seat_id)")){
-			
-		$result = mysqli_query($con,"SELECT * FROM bookings WHERE show_id=".$show_id." AND timing_id=".$timing." AND seat_id=".$seat_id);
-	
-		$row = mysqli_fetch_all($result);
+		$row = mysqli_fetch_all($res);
 		
 		if(count($row)>0){
-			echo $row[0][0];
+			echo 'booked';
+		}else{
+
+				if(mysqli_query($con, "INSERT INTO bookings (`show_id`, `timing_id`, `seat_id`) VALUES ($show_id, $timing, $seat_id)")){
+			
+					$result = mysqli_query($con,"SELECT * FROM bookings WHERE show_id=".$show_id." AND timing_id=".$timing." AND seat_id=".$seat_id);
+				
+					$row = mysqli_fetch_all($result);
+					
+					if(count($row)>0){
+						echo $row[0][0];
+					}
+				}else{
+					echo "error";
+				}
+
+
 		}
-	}else{
-		echo "error";
-	}
+		
+
+
+
+	
 	
 	
 mysqli_close($con); 	
